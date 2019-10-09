838524fa6667c1e9f59bf313e33cf0e7cb2b58ea
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-09 12:37:46 -0400

    add of strcat and strncat

--- my_strncat.c
+++ my_strncat.c
@@ -1,22 +1,22 @@
 /*
 ** EPITECH PROJECT, 2019
-** stract
+** header
 ** File description:
-** je sais pas
+** 
 */
 
 int my_strlen(char const *str);
 
-char *my_strcat(char *dest, char const *src)
+char *my_strncat(char *dest, char const *src, int nb)
 {
     int i = 0;
 
     int lon1 = my_strlen(dest);
     int lon2 = my_strlen(src);
-    while (lon1 + i <= lon1 + lon2) {
-        dest[lon1 + i] = src[i];
+    while (lon1 + i <= lon1 +lon2 && i < nb) {
+        dest[lon1 + i ] = src[i];
         i++;
     }
     dest[lon1 + i] = '\0';
     return (dest);
-}
+}           
