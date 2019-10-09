f2364ceb65f2dbef186c4f92595cde574f0e009c
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-09 13:43:23 -0400

    add of task 04

---
+++ task04/main_my_params.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** lol
+** File description:
+** lol
+*/
+
+void my_putchar(char c);
+int my_putstr(char const* c);
+
+int main(int argc, char **argv)
+{
+    int i =0;
+
+    while (i <= argc - 1) {
+        my_putstr(argv[i]);
+        my_putchar('\n');
+        i++;
+    }
+}
+        
